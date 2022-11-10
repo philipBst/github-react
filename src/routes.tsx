2a1f7c0a24c2552d2a1d75bc65235ae1a382ae5c
philip.basnet <philip.basnet@dzangolab.com>
2022-11-10 15:34:34 +0545

    feat: update route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,15 @@
 import { Navigate, RouteObject } from "react-router-dom";
 
+import { Search } from "pages";
+
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <Navigate to="/search" />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
   },
 ];
 
