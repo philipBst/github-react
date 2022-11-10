8428f969565c86ad8991a8c72f5a768a606382f9
philip.basnet <philip.basnet@dzangolab.com>
2022-11-10 21:54:21 +0545

    feat: create search page ui with example data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
-import { SearchBox } from "../components";
+import { SearchBox, Header } from "../components";
+import RepoCard from "../components/RepoCard";
 
 const Home: React.FC = () => (
-  <div className="w-full h-full">
-    <div className="container">
+  <div className="w-full h-full flex flex-col justify-center">
+    <div className="w-11/12 md:w-5/12 p-5 mx-auto">
+      <Header />
       <SearchBox />
     </div>
+    <ul className="w-11/12 md:w-5/12 p-5 mx-auto">
+      {Array(10)
+        .fill(null)
+        .map((i) => (
+          <RepoCard />
+        ))}
+    </ul>
   </div>
 );
 
