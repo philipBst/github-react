8079b85492cd7727cdfddb6f85086edfd79657ad
philip.basnet <philip.basnet@dzangolab.com>
2022-11-10 20:56:57 +0545

    feat: link home component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import { RouteObject } from "react-router-dom";
 
+import { HomePage } from "./pages";
+
 const routes: RouteObject[] = [
   {
     path: "/",
-    element: <>Home</>,
+    element: <HomePage />,
   },
 ];
 
