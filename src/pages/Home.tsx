284ee00a2611ca485eb611e079e3365293700d6f
philip.basnet <philip.basnet@dzangolab.com>
2022-11-11 14:10:13 +0545

    feat: finish home page ui and actions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { useState, useCallback, useEffect } from "react";
+import { GrPrevious, GrNext } from "react-icons/gr";
 
 import { Container, AppHeader, SearchBox, RepoCard } from "../components";
 
 import { searchReposBy } from "../api/methods";
 import { maxResultsPerFetch } from "../constants";
+import { isEmptyArray } from "../utils/array-utils";
 
 import type { IRepoData } from "../interfaces";
 import type { Sort } from "../types";
@@ -39,7 +41,7 @@ const Home = () => {
     );
     setData(data);
     setPage((page) => page + 1);
-  }, []);
+  }, [query, sortBy, page]);
 
   const prevPage = useCallback(async () => {
     const data = await searchReposBy(
@@ -50,7 +52,7 @@ const Home = () => {
     );
     setData(data);
     setPage((page) => page - 1);
-  }, []);
+  }, [query, sortBy, page]);
 
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4">
@@ -67,6 +69,28 @@ const Home = () => {
           ))}
         </ul>
       </Container>
+      {!isEmptyArray(data.items) && (
+        <Container>
+          <div className="w-full flex items-center justify-between pt-4 border-t-2 border-gray-400">
+            <span className="text-xs text-gray-400">
+              Total Results: {data.total_count || 0} | Showing{" "}
+              {maxResultsPerFetch} results
+            </span>
+            <div className="flex items-center justify-between gap-4 max-w-max">
+              <button className="bg-white p-1 rounded-md" onClick={prevPage}>
+                <GrPrevious />
+              </button>
+              <span>
+                Page {page} of{" "}
+                {Math.floor(data.total_count / maxResultsPerFetch)}
+              </span>
+              <button className="bg-white p-1 rounded-md" onClick={nextPage}>
+                <GrNext />
+              </button>
+            </div>
+          </div>
+        </Container>
+      )}
     </div>
   );
 };
