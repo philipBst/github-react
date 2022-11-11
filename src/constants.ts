c73d788b4cdcdf9cbbf98532da2b312cc1fdc643
philip.basnet <philip.basnet@dzangolab.com>
2022-11-11 11:07:23 +0545

    feat: update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,9 @@
+import { Sortable } from "./types";
+
 export const APP_NAME = "RepoFinder";
-export const SORT_TYPES = ["Stars", "Forks", "Help Wanted Issues", "Updated"];
+export const SORT_TYPES: Sortable[] = [
+  { name: "Stars", value: "stars" },
+  { name: "Forks", value: "forks" },
+  { name: "Help Wanted Issues", value: "help-wanted-issues" },
+  { name: "Updated", value: "updated" },
+];
