652d95cf2fcecaae06a0e3d98209aafc769b4d6a
philip.basnet <philip.basnet@dzangolab.com>
2022-11-11 16:50:09 +0545

    feat: change card component for fault tolerance

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import Container from "./Container";
 
 const Card: React.FC<React.PropsWithChildren> = ({ children }) => (
   <Container>
-    <div className="w-full min-h-max p-4 rounded-md overflow-hidden drop-shadow-lg">
+    <div className="w-full min-h-max p-4 rounded-md overflow-hidden drop-shadow-lg bg-slate-800">
       {children}
     </div>
   </Container>
