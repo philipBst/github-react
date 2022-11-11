623de3eafe976fc560ca2ad43d40c8a2874288b8
philip.basnet <philip.basnet@dzangolab.com>
2022-11-11 14:57:10 +0545

    feat: add method for fetching repo details

--- src/api/methods.ts
+++ src/api/methods.ts
@@ -1,4 +1,4 @@
-import { GITHUB_SEARCH_REPOS_URL } from "./endpoints";
+import { GITHUB_SEARCH_REPOS_URL, GITHUB_GET_REPO_URL } from "./endpoints";
 
 import type { IRepoData } from "../interfaces";
 import type { Sort } from "../types";
@@ -12,5 +12,10 @@ export const searchReposBy = async (
   const response = await fetch(
     GITHUB_SEARCH_REPOS_URL(q, per_page, sort, page)
   );
+  return response.json();
+};
+
+export const getRepo = async (owner: string, repoName: string) => {
+  const response = await fetch(GITHUB_GET_REPO_URL(owner, repoName));
   return response.json();
 };
